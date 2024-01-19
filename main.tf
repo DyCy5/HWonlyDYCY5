071a613958ec0dc999d35d41f93cf394054fbfc1
dycy5 <demaris.smith@gmail.com>
2024-01-18 19:56:03 -0500

    main

--- main.tf
+++ main.tf
@@ -1,7 +1,11 @@
 # Create a virtual network
 resource "azurerm_virtual_network" "vnet" {
-  name                = "myTFVnet"
+  name                = var.resource_group_name
   address_space       = ["10.0.0.0/16"]
   location            = "westus2"
   resource_group_name = azurerm_resource_group.rg.name
+  tags = {
+    Environment = "Terraform Getting Started"
+    Team        = "DevOps"
+  }
 }
